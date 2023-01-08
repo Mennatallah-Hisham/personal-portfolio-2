ef0fff4e421febb808e3f8ec306ba1f2d2066a5e
unknown <mennahisham1d@gmail.com>
2023-01-08 14:29:40 +0200

    changes

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ const progressBarContainer =document.querySelector(".skills");
 const counterContainer =document.querySelector(".info");
 const counters = document.querySelectorAll(".num");
 
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav-list-link");
+
+
 function progressUp(){
     progressBars.forEach(bar => 
     
@@ -37,7 +41,7 @@ function progressUp(){
 function countUp (){
 counters.forEach(counter=>{
     const target = counter.getAttribute("data-target");
-    console.log(target)
+   
   let current =0;
   function updateCounter(){
 
@@ -88,3 +92,46 @@ const counterObs = new IntersectionObserver(startCouterUp,obsOptions);
 counterObs.observe(counterContainer);
 
 
+
+
+const sectionLinkActivation=function (entery){
+    const section =entery[0];
+
+const sectionId =section.target.getAttribute("id");
+if(!sectionId) return;
+    if(section.isIntersecting){
+     
+     
+        
+        
+
+     document.querySelector(`[href="#${sectionId}"]`)?.classList.add('active-link');
+    
+
+     
+
+    }
+    if(section.isIntersecting ===false){
+       
+        document.querySelector(`[href="#${sectionId}"]`)?.classList.remove('active-link');
+    }
+
+
+    
+   
+
+}
+
+const sectionObs = new IntersectionObserver(sectionLinkActivation,{
+   
+        root:null,
+        threshold:0.1,
+    
+})
+
+
+
+sections.forEach((section)=>{
+    sectionObs.observe(section);
+
+})
